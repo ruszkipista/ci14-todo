09620849692b3938a54d871eaf9cd0dfa2b21faf
ruszkipista <oi5672@gmail.com>
2021-09-09 16:46:50 +0100

    add Item table render to todoList.html

--- todo/models.py
+++ todo/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(default=False, null=False, blank=False)
@@ -9,3 +10,4 @@ class Item(models.Model):
     # redefine how an Item object is represented (by its name)
     def __str__(self):
         return self.name
+        
