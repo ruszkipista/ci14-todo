6209ca952a76de34560e0234995912b445224e86
ruszkipista <oi5672@gmail.com>
2021-09-21 15:07:11 +0100

    add Procfile and requirements.txt for Heroku deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ if os.path.exists("env.py"):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n!5&!5dkaf^=hkxr0k(&e0t)o%!7r&%q@chwqnym0z0xgkltab'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
